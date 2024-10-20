68f37cbf955a48c082a3b0762a72bf9f18c86f75
Lai Hong Yu <joyjay606@gmail.com>
2024-10-20 15:16:58 +0800

    Simplification of navigation bar implementation

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-<script>
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-        const answer = button.nextElementSibling;
-        answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+// <script>
+// document.querySelectorAll('.faq-question').forEach(button => {
+//     button.addEventListener('click', () => {
+//         const answer = button.nextElementSibling;
+//         answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+//     });
+// });
+// </script>
+function includeHTML() {
+    const elements = document.querySelectorAll("[data-include]");
+    elements.forEach(async (el) => {
+        const file = el.getAttribute("data-include");
+        const response = await fetch(file);
+        if (response.ok) {
+            el.innerHTML = await response.text();
+        }
     });
-});
-</script>
+}
+document.addEventListener("DOMContentLoaded", includeHTML);
