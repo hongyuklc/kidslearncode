abf485ed05a9b192cfc6124b277581f99b91a7e4
Lai Hong Yu <joyjay606@gmail.com>
2024-11-16 18:12:36 +0800

    Accounts fixed

--- IDE/auth.js
+++ IDE/auth.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js'
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDMDuqV-p_QwykhdCV1kjbWgg0VC6AiX1o",
